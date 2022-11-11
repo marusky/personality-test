e08c3a6112209e03ddf60022aa65beb66d9a9f42
marusky <adrianmarusakza@gmail.com>
2022-11-11 22:06:21 +0100

    Update seeds (main)

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,85 @@ characteristics = [
     "Má rád zábavu",
     "Prispôsobivý",
     "Detailista"
-  ]
+  ],
+  [
+    "Cieľavedomý",
+    "Spontánny",
+    "Hĺbavý",
+    "Zvedavý"
+  ],
+  [
+    "Má silnú vôľu",
+    "Má rád nové nápady",
+    "Trpezlivý",
+    "Vytrvalý"
+  ],
+  [
+    "Samostatný",
+    "Optimistický",
+    "Dobrý poslucháč",
+    "Citlivý"
+  ],
+  [
+    "Vytrvalý",
+    "Dokáže riskovať",
+    "Lojálny",
+    "Presný"
+  ],
+  [
+    "Preberá vedenie",
+    "Motivátor",
+    "Stojí pevne na oboch nohách",
+    "Kontrolujúci sa"
+  ],
+  [
+    "Odhodlaný",
+    "Veľmi výrečný",
+    "Ľahko sa poddá",
+    "Predvídateľný"
+  ],
+  [
+    "Podnikavý",
+    "Priateľský",
+    "Nerozhodný",
+    "Svedomitý"
+  ],
+  [
+    "Súťaživý",
+    "Populárny",
+    "Nemá rád zmeny",
+    "Vie posúdiť veci"
+  ],
+  [
+    "Produktívny",
+    "Má rád rôznorodosť",
+    "Empatický",
+    "Analytický"
+  ],
+  [
+    "Rozhodný",
+    "Zameraný na skupinu",
+    "Starostlivý",
+    "Precízny"
+  ],
+  [
+    "Dobrodružný",
+    "Iniciátor",
+    "Tolerantný",
+    "Rád funguje podľa plánu"
+  ],
+  [
+    "Nezávislý",
+    "Inšpirujúci",
+    "Prináša pokoj",
+    "Zámerný"
+  ],
+  [
+    "Podnikavý",
+    "Má rád zmeny",
+    "Suchý humor",
+    "Usporiadaný"
+  ],
 ]
 
 characteristics.each do |line|
